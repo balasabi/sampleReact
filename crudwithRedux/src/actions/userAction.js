f302cbfb771d0b15ae9e0665daaa3b348116716e
Balas <balas@kenlasystems.com>
2022-02-20 10:45:30 +0530

    dfasdfas

--- crudwithRedux/src/actions/userAction.js
+++ crudwithRedux/src/actions/userAction.js
@@ -3,13 +3,13 @@ import {FETCH_USERS} from './actionTypes';
 
  export function fetchUsers() {
     return dispatch => {
-         fetch("http://localhost:4000/", {
-         method: "GET"})
-       .then(response => response.json())
-         .then(response => {
-            if(response !== undefined && response.status ==="success") {
-              this.props.dispatch({type:USER_ATTACHMENT,data:response.payload})   
-           }
+// //          fetch("http://localhost:4000/", {
+// //          method: "GET"})
+// //        .then(response => response.json())
+// //          .then(response => {
+// //             if(response !== undefined && response.status ==="success") {
+//               this.props.dispatch({type:USER_ATTACHMENT,data:response.payload})   
+//            }
       })
     }
 }
